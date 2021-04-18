0b1efb324ff7b5513609d6a38b41ab5fb929283c
Sekwah <sekwah41@gmail.com>
2021-04-18 03:04:53 +0100

    Altered convert script

--- imageconvert/convert.bat
+++ imageconvert/convert.bat
@@ -1,3 +1,4 @@
-ffmpeg -i "%~1" -vf "scale='min(600,iw)':'min(448,ih)'" "%~dp0/scaled.png"
-ffmpeg -i "%~dp0/scaled.png" -i "%~dp0/palette.png" -filter_complex "paletteuse" output.bmp
+ffmpeg -i "%~1" -vf "scale='min(600,iw)':'min(448,ih)':force_original_aspect_ratio=decrease" "%~dp0/scaled.png"
+ffmpeg -i "%~dp0/scaled.png" -i "%~dp0/palette.png" -filter_complex "paletteuse" "%~n1.bmp"
 del "%~dp0/scaled.png"
+pause
