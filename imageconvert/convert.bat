706c79408e237116f65281258b07b6a9d50e78d2
Sekwah <sekwah41@gmail.com>
2021-04-18 03:05:24 +0100

    remove pause

--- imageconvert/convert.bat
+++ imageconvert/convert.bat
@@ -1,4 +1,3 @@
 ffmpeg -i "%~1" -vf "scale='min(600,iw)':'min(448,ih)':force_original_aspect_ratio=decrease" "%~dp0/scaled.png"
 ffmpeg -i "%~dp0/scaled.png" -i "%~dp0/palette.png" -filter_complex "paletteuse" "%~n1.bmp"
 del "%~dp0/scaled.png"
-pause
